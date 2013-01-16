eb6fd125a579829f56cec88d13e3235db3c92743
doctorxyz <doctorxyz@localhost>
2013-01-16 03:29:17 -0200

    GSModeSelector v0.36b (2013.01.16) by doctorxyz - GUI can be bootable and relaunchable by FCMB E1 launch keys (due to DeInitGSM, New IOP reboot sequence, simplified Makefile, new Launcher based on uLE v4.42a, ...)

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
+.SILENT:
+
 PACKAGE = GSM
 EE_BIN = $(PACKAGE).ELF
-EE_OBJS := iomanx.o filexio.o ps2fs.o fakehost.o loader.o elf.o
+EE_OBJS := engine.o gsm.o elf.o loader.o
+EE_OBJS += iomanx.o filexio.o ps2fs.o fakehost.o
 EE_OBJS += timer.o pad.o
-EE_OBJS += engine.o  
-EE_OBJS += gsm.o  
 	
-EE_INCS += -I$(PS2SDK)/ports/include -I$(GSKIT)/include -I$(GSKIT)/ee/dma/include -I$(GSKIT)/ee/gs/include -I$(GSKIT)/ee/toolkit/include
+EE_INCS +=  -I$(PS2SDK)/ee/include -I$(PS2SDK)/ports/include -I$(GSKIT)/include -I$(GSKIT)/ee/dma/include -I$(GSKIT)/ee/gs/include -I$(GSKIT)/ee/toolkit/include
 
-EE_LIBS = -L$(PS2SDK)/ports/lib -L$(GSKIT)/lib -lgskit -ldmakit -ljpeg
-EE_LIBS += -lfileXio -lmc -lpad
+EE_LIBS = -lmc -lpad -lfileXio -lpatches -ldebug -lc -lkernel -L$(GSKIT)/lib -lgskit -ldmakit
 
-EE_LDFLAGS = -Xlinker -Map -Xlinker 'uncompressed $(PACKAGE).map' -L$(PS2SDK)/ee/lib -Tlinkfile -nostartfiles
+EE_LDFLAGS =  -nostartfiles -Tlinkfile -L$(PS2SDK)/ee/lib -L$(PS2SDK)/sbv/lib -s
 
 all: $(EE_BIN)
 	 rm -f 'uncompressed $(PACKAGE).ELF'
@@ -40,26 +40,27 @@ clean:
 	rm -f *.ELF *.o *.a *.s *.i *.map
 	$(MAKE) -C loader clean
 
-rebuild: clean all
+rebuild:clean all
+
+release:rebuild
+	rm -f 'uncompressed $(PACKAGE).ELF' *.o *.a *.s *.i *.map
+	$(MAKE) -C loader clean
 
 iomanx.s:
-	bin2s irx/iomanX.irx iomanx.s iomanx_irx
+	bin2s $(PS2SDK)/iop/irx/iomanX.irx iomanx.s iomanx_irx
         
 filexio.s:
-	bin2s irx/fileXio.irx filexio.s filexio_irx
+	bin2s $(PS2SDK)/iop/irx/fileXio.irx filexio.s filexio_irx
             
 ps2fs.s:
-	bin2s irx/ps2fs.irx ps2fs.s ps2fs_irx
+	bin2s $(PS2SDK)/iop/irx/ps2fs.irx ps2fs.s ps2fs_irx
 
 fakehost.s:
-	bin2s irx/fakehost.irx fakehost.s fakehost_irx
+	bin2s $(PS2SDK)/iop/irx/fakehost.irx fakehost.s fakehost_irx
 	
 loader.s:
 	$(MAKE) -C loader
 	bin2s loader/loader.elf loader.s loader_elf
-
-splash.s:
-	bin2s splash.jpg splash.s splash		
 
 include $(PS2SDK)/samples/Makefile.pref
 include $(PS2SDK)/samples/Makefile.eeglobal
--- elf.c
+++ elf.c
@@ -53,16 +53,217 @@ typedef struct
 	u32	flags;
 	u32	align;
 } elf_pheader_t;
+
+#define MOUNT_LIMIT 4
+#define MAX_NAME 256
+char mountedParty[MOUNT_LIMIT][MAX_NAME];
+int gen_fd[256]; //Allow up to 256 generic file handles
+int gen_io[256]; //For each handle, also memorize io type
+int	latestMount = -1;
+int fileMode =  FIO_S_IRUSR | FIO_S_IWUSR | FIO_S_IXUSR | FIO_S_IRGRP | FIO_S_IWGRP | FIO_S_IXGRP | FIO_S_IROTH | FIO_S_IWOTH | FIO_S_IXOTH;
+
 //--------------------------------------------------------------
 //End of data declarations
+
 //--------------------------------------------------------------
 //Start of function code
+
+//--------------------------------------------------------------
+void unmountParty(int party_ix)
+{
+	char pfs_str[6];
+
+	strcpy(pfs_str, "pfs0:");
+	pfs_str[3] += party_ix;
+	if(fileXioUmount(pfs_str) < 0)
+		return; //leave variables unchanged if unmount failed (remember true state)
+	if(party_ix < MOUNT_LIMIT){
+		mountedParty[party_ix][0] = 0;
+	}
+	if(latestMount==party_ix)
+		latestMount=-1;
+}
+
+//--------------------------------------------------------------
+int mountParty(const char *party)
+{
+	int i, j;
+	char pfs_str[6];
+
+	for(i=0; i<MOUNT_LIMIT; i++){ //Here we check already mounted PFS indexes
+		if(!strcmp(party, mountedParty[i]))
+			goto return_i;
+	}
+
+	for(i=0, j=-1; i<MOUNT_LIMIT; i++){ //Here we search for a free PFS index
+		if(mountedParty[i][0] == 0){
+			j=i;
+			break;
+		}
+	}
+
+	if(j == -1){ //Here we search for a suitable PFS index to unmount
+		for(i=0; i<MOUNT_LIMIT; i++){
+			if(i!=latestMount){
+				j=i;
+				break;
+			}
+		}
+		unmountParty(j);
+	}
+	//Here j is the index of a free PFS mountpoint
+	//But 'free' only means that the main uLE program isn't using it
+	//If the ftp server is running, that may have used the mountpoints
+
+	//RA NB: The old code to reclaim FTP partitions was seriously bugged...
+
+	i = j;
+	strcpy(pfs_str, "pfs0:");
+
+	pfs_str[3] = '0'+i;
+	if(fileXioMount(pfs_str, party, FIO_MT_RDWR) < 0){ //if FTP stole it
+		for(i=0; i<=4; i++){ //for loop to kill FTP partition mountpoints
+			if(i!=latestMount){ //if unneeded by uLE
+				unmountParty(i);  //unmount partition mountpoint
+				pfs_str[3] = '0'+i; //prepare to reuse that mountpoint
+				if(fileXioMount(pfs_str, party, FIO_MT_RDWR) >= 0)
+					break; //break from the loop on successful mount
+			} //ends if unneeded by uLE
+		} //ends for loop to kill FTP partition mountpoints
+		//Here i indicates what happened above with the following meanings:
+		//0..4==Success after trying i mountpoints,  5==Failure
+		if(i>4)
+			return -1;
+	} //ends if clause for mountpoints stolen by FTP
+	strcpy(mountedParty[i], party);
+return_i:
+	latestMount = i;
+	return i;
+}
+
+//--------------------------------------------------------------
+void genLimObjName(char *uLE_path, int reserve)
+{
+	char	*p, *q, *r;
+	int	limit = 256; //enforce a generic limit of 256 characters
+	int	folder_flag = (uLE_path[strlen(uLE_path)-1] == '/'); //flag folder object
+	int overflow;
+
+	if(!strncmp(uLE_path, "mc", 2) || !strncmp(uLE_path, "vmc", 3))
+		limit = 32;    //enforce MC limit of 32 characters
+
+	if(folder_flag)                  //if path ends with path separator
+		uLE_path[strlen(uLE_path)-1]=0;  //  remove final path separator (temporarily)
+
+	p = uLE_path;    //initially assume a pure object name (quite insanely :))
+	if((q=strchr(p, ':')) != NULL)   //if a drive separator is present
+		p = q+1;                       //  object name may start after drive separator
+	if((q=strrchr(p, '/')) != NULL)  //If there's any path separator in the string
+		p = q+1;                       //  object name starts after last path separator
+	limit -= reserve;                //lower limit by reserved character space
+	overflow = strlen(p)-limit;      //Calculate length of string to remove (if positive)
+	if((limit<=0)||(overflow<=0))    //if limit invalid, or not exceeded
+		goto limited;                  //  no further limitation is needed
+	if((q=strrchr(p, '.')) == NULL)  //if there's no extension separator
+		goto limit_end;                //limitation must be done at end of full name
+	r = q-overflow;                  //r is the place to recopy file extension
+	if(r > p){                       //if this place is above string start
+		strcpy(r, q);                  //remove overflow from end of prefix part
+		goto limited;                  //which concludes the limitation
+	}//if we fall through here, the prefix part was too short for the limitation needed
+limit_end:
+	p[limit] = 0;                  //  remove overflow from end of full name
+limited:
+
+	if(folder_flag)                  //if original path ended with path separator
+		strcat(uLE_path, "/");         //  reappend final path separator after name
+}
+//------------------------------
+//endfunc genLimObjName
+
+//--------------------------------------------------------------
+int genOpen(char *path, int mode)
+{
+	int i, fd, io;
+
+	genLimObjName(path, 0);
+	for(i=0; i<256; i++)
+		if(gen_fd[i] < 0)
+			break;
+	if(i > 255)
+		return -1;
+
+	if(!strncmp(path, "pfs", 3)){
+		fd = fileXioOpen(path, mode, fileMode);
+		io = 1;
+	}else{
+		fd = fioOpen(path, mode);
+		io = 0;
+	}
+	if(fd < 0)
+		return fd;
+
+	gen_fd[i] = fd;
+	gen_io[i] = io;
+	return i;
+}
+//------------------------------
+//endfunc genOpen
+
+//--------------------------------------------------------------
+int genLseek(int fd, int where, int how)
+{
+	if((fd < 0) || (fd > 255) || (gen_fd[fd] < 0))
+		return -1;
+
+	if(gen_io[fd])
+		return fileXioLseek(gen_fd[fd], where, how);
+	else
+		return fioLseek(gen_fd[fd], where, how);
+}
+//------------------------------
+//endfunc genLseek
+
+//--------------------------------------------------------------
+int genRead(int fd, void *buf, int size)
+{
+	if((fd < 0) || (fd > 255) || (gen_fd[fd] < 0))
+		return -1;
+
+	if(gen_io[fd])
+		return fileXioRead(gen_fd[fd], buf, size);
+	else
+		return fioRead(gen_fd[fd], buf, size);
+}
+//------------------------------
+//endfunc genRead
+
+//--------------------------------------------------------------
+int genClose(int fd)
+{
+	int ret;
+
+	if((fd < 0) || (fd > 255) || (gen_fd[fd] < 0))
+		return -1;
+
+	if(gen_io[fd]==1)
+		ret = fileXioClose(gen_fd[fd]);
+	else if(gen_io[fd]==0)
+		ret = fioClose(gen_fd[fd]);
+	else
+		return -1;
+	gen_fd[fd] = -1;
+	return ret;
+}
+//------------------------------
+//endfunc genClose
+
 //--------------------------------------------------------------
 // checkELFheader Tests for valid ELF file 
 // Modified version of loader from Independence
 //	(C) 2003 Marcus R. Brown <mrbrown@0xd6.org>
 //--------------------------------------------------------------
-int checkELFheader(const char *path)
+int checkELFheader(char *path)
 {
 	elf_header_t elf_head;
 	u8 *boot_elf = (u8 *) &elf_head;
@@ -71,49 +272,51 @@ int checkELFheader(const char *path)
 	char fullpath[MAX_PATH], tmp[MAX_PATH], *p;
 
 	strcpy(fullpath,path);
-	if(!strncmp(fullpath, "hdd0", 4)) {
-		sprintf(tmp, "hdd0:%s", &path[6]);
+	if(	!strncmp(fullpath, "mc", 2)
+		||!strncmp(fullpath, "rom", 3)
+		||!strncmp(fullpath, "cdrom", 5)
+		||!strncmp(fullpath, "cdfs", 4)
+		){; //fullpath is already correct
+	}else if(!strncmp(fullpath, "hdd0:", 5)) {
+		p = &path[5];
+		if(*p == '/')
+			p++;
+		sprintf(tmp, "hdd0:%s", p);
 		p = strchr(tmp, '/');
 		sprintf(fullpath, "pfs0:%s", p);
 		*p = 0;
-		ret = fileXioMount("pfs0:", tmp, FIO_MT_RDONLY);
-		if ((fd = fileXioOpen(fullpath, O_RDONLY, FIO_S_IRUSR | FIO_S_IWUSR | FIO_S_IXUSR | FIO_S_IRGRP | FIO_S_IWGRP | FIO_S_IXGRP | FIO_S_IROTH | FIO_S_IWOTH | FIO_S_IXOTH)) < 0){
-			fileXioUmount("pfs0:");
+		if( (ret = mountParty(tmp)) < 0)
 			goto error;
-		}
-		size = fileXioLseek(fd, 0, SEEK_END);
-		if (!size){
-			fileXioClose(fd);
-			fileXioUmount("pfs0:");
-			goto error;
-		}
-		fileXioLseek(fd, 0, SEEK_SET);
-		fileXioRead(fd, boot_elf, sizeof(elf_header_t));
-		fileXioClose(fd);
-		fileXioUmount("pfs0:");
-	}else if(!strncmp(fullpath, "mc", 2) ||
-		!strncmp(fullpath, "mass", 4) ||
-		!strncmp(fullpath, "host", 4)){
-		if ((fd = fioOpen(fullpath, O_RDONLY)) < 0) 
-			goto error;
-		size = fioLseek(fd, 0, SEEK_END);
-		if (!size){
-			fioClose(fd);
-			goto error;
-		}
-		fioLseek(fd, 0, SEEK_SET);
-		fioRead(fd, boot_elf, sizeof(elf_header_t));
-		fioClose(fd);
+		fullpath[3] += ret;
+	}else if(!strncmp(fullpath, "mass", 4)){
+		char *pathSep;
+
+		pathSep = strchr(path, '/');
+		if(pathSep && (pathSep-path<7) && pathSep[-1]==':')
+			strcpy(fullpath+(pathSep-path), pathSep+1);
+	}else if(!strncmp(fullpath, "host:", 5)){
+		if(path[5] == '/')
+			strcpy(fullpath+5, path+6);
 	} else {
-		return 0;
+		return 0;  //return 0 for unrecognized device
 	}
+	if ((fd = genOpen(fullpath, O_RDONLY)) < 0) 
+		goto error;
+	size = genLseek(fd, 0, SEEK_END);
+	if (!size){
+		genClose(fd);
+		goto error;
+	}
+	genLseek(fd, 0, SEEK_SET);
+	genRead(fd, boot_elf, sizeof(elf_header_t));
+	genClose(fd);
 
 	if ((_lw((u32)&eh->ident) != ELF_MAGIC) || eh->type != 2)
 		goto error;
 	
-	return 1;
+	return 1;  //return 1 for successful check
 error:
-	return -1;
+	return -1; //return -1 for failed check
 }
 //------------------------------
 //End of func:  int checkELFheader(const char *path)
@@ -136,7 +339,7 @@ void RunLoaderElf(char *filename, char *party)
 		char fakepath[128], *p;
 		if(0 > fileXioMount("pfs0:", party, FIO_MT_RDONLY)){
 			//Some error occurred, it could be due to something else having used pfs0
-			fileXioUmount("pfs0:");  //So we try unmounting pfs0, to try again
+			unmountParty(0);  //So we try unmounting pfs0, to try again
 			if(0 > fileXioMount("pfs0:", party, FIO_MT_RDONLY))
 				return;  //If it still fails, we have to give up...
 		}
